246b1da25a2e8402e63002fc16b0626dd3fb4266
Dagzy <quincye.williams@gmail.com>
2017-06-13 14:28:37 -0400

    added selectors that work!

--- 0.01_selectors.js
+++ 0.01_selectors.js
@@ -3,20 +3,26 @@ alert("jQuery is working")
 $(function(){
 	
 	//By id - this adds a CSS class
-	$("#example-one").addClass('highlight');
+	$('#example-one').addClass('highlight');
 
 	//By class - we'll make two things change here.
-	$(".example-two").css("border", "4 px solid gray");
+	$('.example-two').css('border', '4px solid gray');
 
 	//By element
 	$('h5').click(function(){
 		$('h5').fadeOut('slow', 'linear');
+	});
 	
 	//https://jqueryui.com/draggable/
 	//$('#draggable').draggable();
 
-	$('#id-combo-demo, .class-combo-demo, #draggable').draggable();
+	$('#id-combo-demo, .class-combo-demo, #draggable').draggable(){};
 
+	$('li:contains("sad")').click(function(){
+		$(this).hide()
 	});
+
+
+
 
 });
