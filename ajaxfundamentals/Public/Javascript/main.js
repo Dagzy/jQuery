6cfe1115983cd8b6b06afc986c36a524ce6e0deb
Dagzy <quincye.williams@gmail.com>
2017-06-14 16:49:26 -0400

    added node and friendlist

--- ajaxfundamentals/Public/Javascript/main.js
+++ ajaxfundamentals/Public/Javascript/main.js
@@ -2,11 +2,25 @@ $(document).ready(function(){
 	$.ajax({
 		type:'GET',
 		url:'http://swapi.co/api/people/',
-		success: function(data){
+})
+	.done(function(data){
+		console.log(data);
+	})
+	.fail(function(data){
+		alert("Error loading data")
+	})
+
+
+
+
+
+/*		success: function(data){
 			console.log(data.results[0].name);
 		},
 		error: function(){
 			alert("Error loading data.")
-		}
-	})
+		}*/
+		//use .done and .fail
+	
+	console.log("Hey, you guys!");
 });
--- ajaxfundamentals/index.js
+++ ajaxfundamentals/index.js
@@ -1,13 +1,16 @@
+// require is a part of node that helps me import node_modules folder
+//require also pulls from another javascript file with an export
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-
+//this is express
 app.use(express.static(__dirname + '/Public'));
-
+//this is express
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
-
+//This is node's HTTP from line 5
 http.listen(3000, function(){
 	console.log('listening on port 3000');
 });
+//Examine where variables are set to determine what application is being called.
